3c95cbfe16e938a642ea05933bbdbb49d57c0451
Deaboy <danieleandrus@gmail.com>
2015-01-28 08:54:31 -0700

    Added linear contrast and brightness

--- DanProcessor.cpp
+++ DanProcessor.cpp
@@ -126,11 +126,91 @@ bool Menu_DanFunctions_Posterize(Image& image)
 
 bool Menu_DanFunctions_Brighten(Image& image)
 {
-  return false;
+  if (image.IsNull())
+    return false;
+  
+  int rows = image.Height();
+  int cols = image.Width();
+  
+  int brightness;
+  int maxpixel;
+  int minpixel;
+  unsigned char lutable[255] = {0};
+  
+  // Propt user for threshold value
+  if (!Dialog("Brightness").Add(brightness, "Brightness", -255, 255).Show())
+    return false;
+  
+  maxpixel = 255 - brightness;
+  minpixel = 0 - brightness;
+  
+  // Build lookup table
+  for (int i = 0; i < 255; i++)
+    lutable[i] = (unsigned char) (i <= minpixel ? 0 :
+                                  i >= maxpixel ? 255 :
+                                  i + brightness);
+  
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      // Brighten each color
+      image[i][j].SetRGB((lutable[image[i][j].Red()]),
+                         (lutable[image[i][j].Green()]),
+                         (lutable[image[i][j].Blue()]));
+    }
+  }
+  
+  return true;
 }
 
 bool Menu_DanFunctions_LinearContrast(Image& image)
 {
-  return false;
+  if (image.IsNull())
+    return false;
+  
+  int rows = image.Height();
+  int cols = image.Width();
+  
+  int lowbound;
+  int upbound;
+  double scale;
+  unsigned char lutable[255] = {0};
+  
+  // Propt user for threshold value
+  if (!Dialog("Linear Contrast").Add(lowbound, "Minimum Intensity", 0, 255)
+        .add(upbound, "Maximum Intensity", 0, 255).Show())
+    return false;
+  
+  // If reversed, flip 'em
+  if (lowbound > upbound)
+  {
+    lowbound ^= upbound;
+    upbound ^= lowbound;
+    lowbound ^= upbound;
+  }
+  
+  // Calculate scale
+  if (lowbound != upbound)
+    scale = 256 / (upbound - lowbound);
+  
+  // Build lookup table
+  for (int i = 0; i < 255; i++)
+    lutable[i] = (unsigned char) (i <= lowbound ? 0 :
+                                  i >= upbound ? 255 :
+                                  ((i - lowbound) * scale) + 0.5);
+  
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      // Add contrast to each color
+      image[i][j].SetRGB((lutable[image[i][j].Red()]),
+                         (lutable[image[i][j].Green()]),
+                         (lutable[image[i][j].Blue()]));
+    }
+  }
+  
+  return true;
 }
 
