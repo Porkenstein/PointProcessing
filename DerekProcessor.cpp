db715b2fd97c5101db7c53d8379c35d1eea483b8
Stotz <7058670@linux09.mcs.sdsmt.edu>
2015-02-02 18:25:25 -0700

    added some functions to DerekProcessor

--- DerekProcessor.cpp
+++ DerekProcessor.cpp
@@ -1,6 +1,9 @@
 #include "DerekProcessor.h"
+#include <cmath>
+#include <string>
+#include <qfiledialog.h>
 
-bool DerekProcessor::Menu_DerekProcesses_NegateImage(Image& image)
+bool DerekProcessor::Menu_DerekFunctions_ApplyGamma(Image& image)
 {
   static unsigned char memory[3][256] = {{0},{0},{0}};
   static bool lookup_table = false;
@@ -32,3 +35,111 @@ bool DerekProcessor::Menu_DerekProcesses_NegateImage(Image& image)
   return true;
 }
 
+bool DerekProcessor::Menu_DerekFunctions_ApplyLogTransform(Image& image)
+{
+  static unsigned char memory[3][256] = {{0},{0},{0}};
+  static bool lookup_table = false;
+
+  // Build the lookup table
+  if (!lookup_table)
+  {
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 256; j++)
+        memory[i][j] = 255 - (unsigned char) j;
+    lookup_table = true;
+  }
+
+  if (image.IsNull()) return false;
+
+  int rows = image.Height();
+  int cols = image.Width();
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      image[i][j].SetRed(memory[0][image[i][j].Red()]);
+      image[i][j].SetGreen(memory[1][image[i][j].Green()]);
+      image[i][j].SetBlue(memory[2][image[i][j].Blue()]);
+    }
+  }
+
+  return true;
+}
+
+bool DerekProcessor::Menu_DerekFunctions_Apply8LevelPseudocolor(Image& image)
+{
+  static unsigned char memory[3][256] = {{0},{0},{0}};
+  static bool lookup_table = false;
+
+  // Build the lookup table
+  if (!lookup_table)
+  {
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 256; j++)
+        memory[i][j] = 255 - (unsigned char) j;
+    lookup_table = true;
+  }
+
+  if (image.IsNull()) return false;
+
+  int rows = image.Height();
+  int cols = image.Width();
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      image[i][j].SetRed(memory[0][image[i][j].Red()]);
+      image[i][j].SetGreen(memory[1][image[i][j].Green()]);
+      image[i][j].SetBlue(memory[2][image[i][j].Blue()]);
+    }
+  }
+
+  return true;
+}
+
+bool DerekProcessor::Menu_DerekFunctions_ApplyContinuousPseudocolor(Image& image)
+{
+  if (image.IsNull()) return false;
+
+  int rows = image.Height();
+  int cols = image.Width();
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      image[i][j].SetRed();
+      image[i][j].SetGreen();
+      image[i][j].SetBlue();
+    }
+  }
+
+  return true;
+}
+
+bool DerekProcessor::Menu_DerekFunctions_SubtractImage(Image& image)
+{
+  std::string subtractor_filepath = QFileDialog::getOpenFileName().toStdString();
+  Image subtractor(subtractor_filepath);
+
+
+  if (image.IsNull() || subtractor.IsNull()) return false;
+
+  //find the smallest dimension, so we don't step outside of either image
+  int rows = std::min(image.Height(), subtractor.Height());
+  int cols = std::min(image.Width(), subtractor.Width());
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      image[i][j].SetRed(image[i][j].Red() - subtractor[i][j].Red());
+      image[i][j].SetGreen(image[i][j].Green() - subtractor[i][j].Green());
+      image[i][j].SetBlue(image[i][j].Blue() - subtractor[i][j].Blue());
+    }
+  }
+
+  return true;
+}
--- DerekProcessor.h
+++ DerekProcessor.h
@@ -6,6 +6,10 @@ class DerekProcessor : public QObject
   Q_OBJECT;
 
   public slots:
-    bool Menu_DerekProcesses_NegateImage(Image& image);
+    bool Menu_DerekFunctions_ApplyGamma(Image& image);
+    bool Menu_DerekFunctions_ApplyLogTransform(Image& image);
+    bool Menu_DerekFunctions_Apply8LevelPseudocolor(Image& image);
+    bool Menu_DerekFunctions_ApplyContinuousPseudocolor(Image& image);
+    bool Menu_DerekFunctions_SubtractImage(Image& image);
 };
 
